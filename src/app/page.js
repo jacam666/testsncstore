1b18c4014ca2be82e7e6f76e050a859f22ffea99
jacam666 <ja6cam@gmail.com>
2024-06-08 08:31:47 +0100

    styling to footer nad main page

--- src/app/page.js
+++ src/app/page.js
@@ -16,25 +16,18 @@ export default function Home() {
         src="/logo/Logo__3_-removebg-preview.png"
         alt="homepage-image"
         className="mx-auto"
-        />
-      {/* <Image
-        width={300}
-        height={500}
-        src="/logo/Logo__2_-removebg-preview.png"
-        alt="homepage-image"
-        className="mx-auto"
-      /> */}
+      />
       <div className="flex-col">
         <div className="flex flex-col lg:flex-row px-9 ">
           <div className="flex flex-col">
             <div className="flex">
-              <h1 className=" text-5xl md:text-4xl sm:text-6xl font-bold my-9 whitespace-nowrap">
-                Snc <span className="text-4xl">Supplements</span>
+              <h1 className=" text-5xl xl:text-8xl md:text-4xl sm:text-6xl font-bold my-9 text-[#007BFF]  whitespace-nowrap">
+                Snc <span className="text-4xl xl:text-5xl">Supplements</span>
               </h1>
             </div>
-            <div className="separator my-4 h-2 sm:h-2 w-2/5 lg:w-2/5 md:w-2/5 "></div>
+            <div className=" my-4 h-2 sm:h-2 w-2/5 lg:w-2/5 md:w-2/5 bg-[#FFC107] "></div>
             <div className="my-3">
-              <p className="text-2xl text-gray-500 mt-9">
+              <p className="text-2xl text-[#666666] mt-9">
                 SNC Supplements are a range of products designed to help you
                 achieve your goals</p>
             </div>
@@ -49,20 +42,20 @@ export default function Home() {
             />
           </div>
         </div>
-        <div className="flex flex-col text-center lg:py-9 bg-yellow-50">
+        <div className="flex flex-col text-center lg:py-9 bg-[#FFC107] text-gray-800">
           <h1 className="text-3xl font-bold my-8 text-gray-400">WELCOME TO SNC</h1>
           <p className="text-4xl sm:text-6xl font-bold px-2 mx-auto lg:w-2/3">
             Enhance Your Training Experience with High-Quality Products That are Specifically Designed and Tested by Bodybuilders
           </p>
           <div className="flex justify-center m-auto p-9">
-            <p className=" text-gray-500 text-2xl sm:text-3xl lg:w-4/5 sm:w-5/6 p-9 ">
+            <p className=" text-gray-800 text-2xl sm:text-3xl lg:w-4/5 sm:w-5/6 p-9 ">
               An extensive range of nutritional supplements, including proteins, pre-workouts, fat burners, and a variety of other products designed to support your health and fitness goals
             </p>
           </div>
         </div>
         {/* <div className="separator  my-4 h-2 w-4/5 m-auto"></div> */}
       </div>
-      <div className="flex justify-center py-9">
+      <div className="flex justify-center py-9 bg-[#F5F5F5]">
         <Image
           width={400}
           height={400}
@@ -72,57 +65,138 @@ export default function Home() {
         />
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-4 lg:gap-1 item-container p-9">
-        <div className="flex flex-col  mx-2 rounded-xl p-2 bg-white">
-          <div className="flex items-center">
-            <div className="text-4xl icon-1 "><TbSquareNumber1Filled /></div>
-            <h1 className="flex ml-4 font-bold">Proteins</h1>
-            <Link href="/protein" className="homepage-button flex rounded-lg px-4 ml-auto mr-4 ">
-            <button>Shop</button>
-            </Link>
+        {[
+          { icon: <TbSquareNumber1Filled />, title: "Proteins", link: "/protein", description: "Our exceptionally high-quality, nutritious, and delicious range of proteins offers a wide variety of options to cater to your health and taste preferences." },
+          { icon: <TbSquareNumber2Filled />, title: "Pre-workouts", link: "/pre-workout", description: "Our pre workouts are blends of nutrients, vitamins, and ingredients to boost energy, focus, and workout performance." },
+          { icon: <TbSquareNumber3Filled />, title: "Fat Loss", link: "/FatLoss", description: "Our Fat loss supplements can assist people in their weight management endeavors by offering extra support and helping them achieve their fitness objectives." },
+          { icon: <TbSquareNumber4Filled />, title: "Wellbeing", link: "/wellbeing", description: "Our wellness products naturally boost testosterone levels, enhance vitality, and strengthen immune system against environmental stressors." }
+        ].map((item, index) => (
+          <div key={index} className="flex flex-col mx-2 rounded-xl p-2 bg-white shadow-lg">
+            <div className="flex items-center">
+              <div className="text-4xl text-[#007BFF]">{item.icon}</div>
+              <h1 className="flex ml-4 font-bold">{item.title}</h1>
+              <Link href={item.link} className=" flex rounded-lg px-4 ml-auto mr-4 bg-[#FF5722] text-white hover:bg-[#FFC107]">
+                <button>Shop</button>
+              </Link>
+            </div>
+            <div>
+              <p className="px-4 py-2 text-[#666666]">{item.description}</p>
+            </div>
           </div>
-          <div>
-            <p className="px-4 py-2">Our exceptionally high-quality, nutritious, and delicious range of proteins offers a wide variety of options to cater to your health and taste preferences.</p>
-          </div>
-        </div>
-        <div className="flex flex-col  mx-2 rounded-xl p-2 bg-white">
-          <div className="flex items-center">
-            <div className="text-4xl icon-1 "><TbSquareNumber2Filled /></div>
-            <h1 className="flex ml-4 font-bold">Pre-workouts</h1>
-            <Link href="/pre-workout" className="homepage-button flex rounded-lg px-4 ml-auto mr-4 ">
-            <button>Shop</button>
-            </Link>
-          </div>
-          <div>
-            <p className="px-4 py-2">Our pre workouts are blends of nutrients, vitamins, and ingredients to boost energy, focus, and workout performance..</p>
-          </div>
-        </div>
-        <div className="flex flex-col  mx-2 rounded-xl p-2 bg-white">
-          <div className="flex items-center">
-            <div className="text-4xl icon-1 "><TbSquareNumber3Filled /></div>
-            <h1 className="flex ml-4 font-bold">Fat Loss</h1>
-            <Link href="/FatLoss" className="homepage-button flex rounded-lg px-4 ml-auto mr-4 ">
-            <button>Shop</button>
-            </Link>
-          </div>
-          <div>
-            <p className="px-4 py-2">Our Fat loss supplements can assist people in their weight management endeavors by offering extra support and helping them achieve their fitness objectives.</p>
-          </div>
-        </div>
-        <div className="flex flex-col  mx-2 rounded-xl p-2 bg-white">
-          <div className="flex items-center">
-            <div className="text-4xl icon-1 "><TbSquareNumber4Filled /></div>
-            <h1 className="flex ml-4 font-bold">Wellbeing</h1>
-            <Link href="/wellbeing" className="homepage-button flex rounded-lg px-4 ml-auto mr-4 ">
-            <button>Shop</button>
-            </Link>
-          </div>
-          <div>
-            <p className="px-4 py-2">Our wellness products naturally boost testosterone levels, enhance vitality, and strengthen immune system against environmental stressors.</p>
-          </div>
-        </div>
+        ))}
       </div>
-      {/* <div className="separator my-5 h-2 w-4/5 mx-auto "></div> */}
-      {/* <div className="separator my-9 h-2 w-4/5 mx-auto "></div> */}
     </div>
   );
 }
+// export default function Home() {
+//   return (
+//     <div className="mt-6 ">
+//       <Image
+//         width={200}
+//         height={200}
+//         src="/logo/Logo__3_-removebg-preview.png"
+//         alt="homepage-image"
+//         className="mx-auto"
+//       />
+//       <div className="flex-col">
+//         <div className="flex flex-col lg:flex-row px-9 ">
+//           <div className="flex flex-col">
+//             <div className="flex">
+//               <h1 className=" text-5xl md:text-4xl sm:text-6xl font-bold my-9  whitespace-nowrap">
+//                 Snc <span className="text-4xl">Supplements</span>
+//               </h1>
+//             </div>
+//             <div className="separator my-4 h-2 sm:h-2 w-2/5 lg:w-2/5 md:w-2/5 "></div>
+//             <div className="my-3">
+//               <p className="text-2xl text-gray-500 mt-9">
+//                 SNC Supplements are a range of products designed to help you
+//                 achieve your goals</p>
+//             </div>
+//           </div>
+//           <div className="flex justify-center lg:justify-end lg:ml-6 lg:py-9">
+//             <Image
+//               width={400}
+//               height={400}
+//               src="/images/homePageImages/homepage-snc-display-image2.jpg"
+//               alt="homepage-image"
+//               className="rounded-3xl my-9  homepage-main-image"
+//             />
+//           </div>
+//         </div>
+//         <div className="flex flex-col text-center lg:py-9 bg-yellow-50">
+//           <h1 className="text-3xl font-bold my-8 text-gray-400">WELCOME TO SNC</h1>
+//           <p className="text-4xl sm:text-6xl font-bold px-2 mx-auto lg:w-2/3">
+//             Enhance Your Training Experience with High-Quality Products That are Specifically Designed and Tested by Bodybuilders
+//           </p>
+//           <div className="flex justify-center m-auto p-9">
+//             <p className=" text-gray-500 text-2xl sm:text-3xl lg:w-4/5 sm:w-5/6 p-9 ">
+//               An extensive range of nutritional supplements, including proteins, pre-workouts, fat burners, and a variety of other products designed to support your health and fitness goals
+//             </p>
+//           </div>
+//         </div>
+//         {/* <div className="separator  my-4 h-2 w-4/5 m-auto"></div> */}
+//       </div>
+//       <div className="flex justify-center py-9">
+//         <Image
+//           width={400}
+//           height={400}
+//           src="/images/homePageImages/snc-products_edited.jpg"
+//           alt="homepage-image"
+//           className="rounded-3xl my-9 w-4/5"
+//         />
+//       </div>
+//       <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-4 lg:gap-1 item-container p-9">
+//         <div className="flex flex-col  mx-2 rounded-xl p-2 bg-white">
+//           <div className="flex items-center">
+//             <div className="text-4xl icon-1 "><TbSquareNumber1Filled /></div>
+//             <h1 className="flex ml-4 font-bold">Proteins</h1>
+//             <Link href="/protein" className="homepage-button flex rounded-lg px-4 ml-auto mr-4 ">
+//             <button>Shop</button>
+//             </Link>
+//           </div>
+//           <div>
+//             <p className="px-4 py-2">Our exceptionally high-quality, nutritious, and delicious range of proteins offers a wide variety of options to cater to your health and taste preferences.</p>
+//           </div>
+//         </div>
+//         <div className="flex flex-col  mx-2 rounded-xl p-2 bg-white">
+//           <div className="flex items-center">
+//             <div className="text-4xl icon-1 "><TbSquareNumber2Filled /></div>
+//             <h1 className="flex ml-4 font-bold">Pre-workouts</h1>
+//             <Link href="/pre-workout" className="homepage-button flex rounded-lg px-4 ml-auto mr-4 ">
+//             <button>Shop</button>
+//             </Link>
+//           </div>
+//           <div>
+//             <p className="px-4 py-2">Our pre workouts are blends of nutrients, vitamins, and ingredients to boost energy, focus, and workout performance..</p>
+//           </div>
+//         </div>
+//         <div className="flex flex-col  mx-2 rounded-xl p-2 bg-white">
+//           <div className="flex items-center">
+//             <div className="text-4xl icon-1 "><TbSquareNumber3Filled /></div>
+//             <h1 className="flex ml-4 font-bold">Fat Loss</h1>
+//             <Link href="/FatLoss" className="homepage-button flex rounded-lg px-4 ml-auto mr-4 ">
+//             <button>Shop</button>
+//             </Link>
+//           </div>
+//           <div>
+//             <p className="px-4 py-2">Our Fat loss supplements can assist people in their weight management endeavors by offering extra support and helping them achieve their fitness objectives.</p>
+//           </div>
+//         </div>
+//         <div className="flex flex-col  mx-2 rounded-xl p-2 bg-white">
+//           <div className="flex items-center">
+//             <div className="text-4xl icon-1 "><TbSquareNumber4Filled /></div>
+//             <h1 className="flex ml-4 font-bold">Wellbeing</h1>
+//             <Link href="/wellbeing" className="homepage-button flex rounded-lg px-4 ml-auto mr-4 ">
+//             <button>Shop</button>
+//             </Link>
+//           </div>
+//           <div>
+//             <p className="px-4 py-2">Our wellness products naturally boost testosterone levels, enhance vitality, and strengthen immune system against environmental stressors.</p>
+//           </div>
+//         </div>
+//       </div>
+//       {/* <div className="separator my-5 h-2 w-4/5 mx-auto "></div> */}
+//       {/* <div className="separator my-9 h-2 w-4/5 mx-auto "></div> */}
+//     </div>
+//   );
+// }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,9 +5,9 @@ import { FaXTwitter } from "react-icons/fa6";
 
 export default function Footer() {
     return (
-        <div className="flex flex-col sm:flex-row justify-evenly mb-9">
-            <div className="text-center text-xl">
-                <h2 className="my-2 text-4xl">Contact us:</h2>
+        <div className="flex flex-col sm:flex-row justify-evenly mb-9 bg-[#f5f5f5] p-6 rounded-lg shadow-lg">
+            <div className="text-center text-xl text-gray-800">
+                <h2 className="my-2 text-4xl font-bold text-[#007BFF]">Contact us:</h2>
                 <p>Moorings,</p>
                 <p>Watling Street,</p>
                 <p>Hockliffe,</p>
@@ -15,12 +15,18 @@ export default function Footer() {
                 <p>LU7 9nb</p>
                 <p>01525 211127</p>
             </div>
-            <div className="text-center text-xl mt-5">
+            <div className="text-center text-xl mt-5 text-gray-800">
                 <h1 className="my-2 text-4xl">Socials</h1>
                 <div className="flex flex-row justify-evenly mt-10 my-auto lg:justify-between text-2xl">
-                    <FaFacebookF className="text-blue-500" />
-                    <FaInstagram className="text-red-800" />
-                    <FaXTwitter />
+                    <a href="https://facebook.com/uksnc" aria-label="Facebook" className="hover:text-[#007BFF]">
+                        <FaFacebookF className="text-blue-600 hover:text-[#007BFF]" />
+                    </a>
+                    <a href="https://instagram.com" aria-label="Instagram" className="hover:text-[#FF5722]">
+                        <FaInstagram className="text-pink-600 hover:text-[#FF5722]" />
+                    </a>
+                    <a href="https://twitter.com/SNC52678131" aria-label="Twitter" className="hover:text-[#1DA1F2]">
+                        <FaXTwitter className="text-blue-400 hover:text-[#1DA1F2]" />
+                    </a>
                 </div>
             </div>
         </div>

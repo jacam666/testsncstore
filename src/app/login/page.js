1e38d7f0b7f9438b592a1a9cbe8a7384911b6945
jacam666 <ja6cam@gmail.com>
2024-06-12 14:58:18 +0100

    styled login and signup pages

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -11,33 +11,35 @@ export default function LoginPage() {
     };
 
     return (
-        <div className="flex justify-center items-center h-screen">
-            <form onSubmit={handleSubmit} className="bg-white p-6 rounded shadow-md w-1/3">
-                <h2 className="text-center text-2xl font-bold mb-5">Login</h2>
-                <div className="mb-4">
-                    <label className="block mb-1" htmlFor="email">Email</label>
-                    <input
-                        type="email"
-                        id="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        className="w-full p-2 border rounded"
-                        required
-                    />
-                </div>
-                <div className="mb-4">
-                    <label className="block mb-1" htmlFor="password">Password</label>
-                    <input
-                        type="password"
-                        id="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        className="w-full p-2 border rounded"
-                        required
-                    />
-                </div>
-                <button type="submit" className="bg-blue-500 text-white p-2 rounded w-full">Login</button>
-            </form>
+        <div className='login-container'>
+            <div className="flex justify-center items-center h-screen">
+                <form onSubmit={handleSubmit} className="bg-white p-6 rounded shadow-md w-5/6 lg:w-1/3">
+                    <h2 className="text-center text-2xl font-bold mb-5">Login</h2>
+                    <div className="mb-4">
+                        <label className="block mb-1" htmlFor="email">Email</label>
+                        <input
+                            type="email"
+                            id="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            className="w-full p-2 border rounded"
+                            required
+                        />
+                    </div>
+                    <div className="mb-4">
+                        <label className="block mb-1" htmlFor="password">Password</label>
+                        <input
+                            type="password"
+                            id="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            className="w-full p-2 border rounded"
+                            required
+                        />
+                    </div>
+                    <button type="submit" className="bg-blue-500 text-white p-2 rounded w-full">Login</button>
+                </form>
+            </div>
         </div>
     );
 }
--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -12,44 +12,46 @@ export default function SignupPage() {
     };
 
     return (
-        <div className="flex justify-center items-center h-screen">
-            <form onSubmit={handleSubmit} className="bg-white p-6 rounded shadow-md w-1/3">
-                <h2 className="text-center text-2xl font-bold mb-5">Sign Up</h2>
-                <div className="mb-4">
-                    <label className="block mb-1" htmlFor="email">Email</label>
-                    <input
-                        type="email"
-                        id="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        className="w-full p-2 border rounded"
-                        required
-                    />
-                </div>
-                <div className="mb-4">
-                    <label className="block mb-1" htmlFor="password">Password</label>
-                    <input
-                        type="password"
-                        id="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        className="w-full p-2 border rounded"
-                        required
-                    />
-                </div>
-                <div className="mb-4">
-                    <label className="block mb-1" htmlFor="confirmPassword">Confirm Password</label>
-                    <input
-                        type="password"
-                        id="confirmPassword"
-                        value={confirmPassword}
-                        onChange={(e) => setConfirmPassword(e.target.value)}
-                        className="w-full p-2 border rounded"
-                        required
-                    />
-                </div>
-                <button type="submit" className="bg-green-500 text-white p-2 rounded w-full">Sign Up</button>
-            </form>
+        <div className='sign-up-container'>
+            <div className="flex justify-center items-center h-screen">
+                <form onSubmit={handleSubmit} className="bg-white p-6 rounded-xl shadow-md w-5/6 lg:w-1/3">
+                    <h2 className="text-center text-3xl font-bold mb-5">Sign Up</h2>
+                    <div className="mb-4">
+                        <label className="block mb-1" htmlFor="email">Email</label>
+                        <input
+                            type="email"
+                            id="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            className="w-full p-2 border rounded"
+                            required
+                        />
+                    </div>
+                    <div className="mb-4">
+                        <label className="block mb-1" htmlFor="password">Password</label>
+                        <input
+                            type="password"
+                            id="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            className="w-full p-2 border rounded"
+                            required
+                        />
+                    </div>
+                    <div className="mb-4">
+                        <label className="block mb-1" htmlFor="confirmPassword">Confirm Password</label>
+                        <input
+                            type="password"
+                            id="confirmPassword"
+                            value={confirmPassword}
+                            onChange={(e) => setConfirmPassword(e.target.value)}
+                            className="w-full p-2 border rounded"
+                            required
+                        />
+                    </div>
+                    <button type="submit" className="bg-green-500 text-white p-2 rounded w-full">Sign Up</button>
+                </form>
+            </div>
         </div>
     );
 }
